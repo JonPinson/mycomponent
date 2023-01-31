f88d0da077db5b2b9d67cd70efc440b36d0edae2
Your Name <you@example.com>
2023-01-31 11:03:52 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Box} from './components/Box';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='container'>
+     <p>Our Clean Slate</p>
+      
+      <Box message="Hi!" />
+      <Box message="Hello!" color='purple'/>
+      <Box message="Hola!" subheading='subheading'/>
+      <Box message="Hey!" />
+      <Box message="Salut!" />
     </div>
   );
 }
